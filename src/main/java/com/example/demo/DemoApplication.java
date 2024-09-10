85c3a1f71b500982bc2b116449c651e73f87512b
CoenDV <c.oen@hotmail.nl>
2024-09-10 09:24:09 +0200

    post

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
@@ -14,6 +16,12 @@ public class DemoApplication {
     String home() {
         return "Hello World!";
     }
+
+
+@PostMapping("/test")
+public String test() {
+    return "Test Post";
+}
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
