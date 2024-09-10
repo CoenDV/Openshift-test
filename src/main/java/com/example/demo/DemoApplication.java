d3a7f1bd8077728715169db7320bcc95e9adaf8e
CoenDV <c.oen@hotmail.nl>
2024-09-10 11:18:25 +0200

    nog meer cors

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,18 +11,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:8081", "https://openshift-web-app-git-coen-de-vries-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com"}, allowedHeaders = "*")
 @RequestMapping("/api")
-@CrossOrigin(allowedHeaders = "*")
 @SpringBootApplication
 public class DemoApplication {
 
     @GetMapping
+    @CrossOrigin(origins = {"http://localhost:8081", "https://openshift-web-app-git-coen-de-vries-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com"}, allowedHeaders = "*")
     public String home() {
         System.out.println("hello");
         return "Hello";
     }
 
     @PostMapping
+    @CrossOrigin(origins = {"http://localhost:8081", "https://openshift-web-app-git-coen-de-vries-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com"}, allowedHeaders = "*")
     public String test() {
         System.out.println("post");
         return "Test Post";
