b1db91991d1f7bb9ee2705204ce9b86b3d010f08
CoenDV <c.oen@hotmail.nl>
2024-09-10 09:28:51 +0200

    api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,15 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 
 @RestController
+@RequestMapping("/api")
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     String home() {
         return "Hello World!";
     }
